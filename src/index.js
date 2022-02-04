27598de96ba35f511298de0b8b203e8aeb287227
Jongmin Choi <quark4904@gmail.com>
2022-02-04 21:12:51 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './styles/GlobalStyle';
+import Theme from './styles/Theme';
+import { ThemeProvider } from 'styled-components';
+
+import Routes from './Routes';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={Theme}>
+      <Routes />
+    </ThemeProvider>
+  </>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById('root'),
+);
